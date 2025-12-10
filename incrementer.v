afc41534a2f5daa9292207eeff2f03a143a794d2
goso2 <seango6604@gmail.com>
2025-12-10 03:18:35 -0800

    Delete incrementer.v

--- incrementer.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns/1ps
-// Incrementer: produces pc+4 (no state inside)
-module incrementer (
-  input  wire [31:0] pcin,
-  output wire [31:0] pcout
-);
-  assign pcout = pcin + 32'd4;
-endmodule
